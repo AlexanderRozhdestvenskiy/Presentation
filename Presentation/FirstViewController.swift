26e82f0d75e0bcf26f2c70b5f534150d3c10fdef
Alexander Rozhdestvenskiy <alexrozh1988@gmail.com>
2021-12-22 14:49:53 +0300

    Data

--- Presentation/FirstViewController.swift
+++ Presentation/FirstViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    let table = UITableView()
+    var model: [Model] = Model.data
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
     }
     
+    func tableViewSetting() {
+        table.delegate = self
+        table.dataSource = self
+    }
+}
 
+extension FirstViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension FirstViewController: UITableViewDelegate {
+    
 }
